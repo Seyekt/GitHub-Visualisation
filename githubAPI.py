894e9ee4e758c70db5a35d6eb39172e7e3b4d17f
Thomas Keyes <keyest@tcd.ie>
2021-01-05 22:30:57 +0000

    Added bar chart to show repo commits

--- githubAPI.py
+++ githubAPI.py
@@ -19,15 +19,13 @@ def repoToStr(repo):
 
 def repoToData(user):
 
-	#user = g.get_user()
-
 	repoData = {}
 
 	repos = user.get_repos()
 
 	for i, repo in enumerate(repos):
 		repoData[i] = {'Repository Name': repo.name, 'Description': repo.description, 'Programming language': repo.language, 'Date created': repo.created_at, 
-		'Date Last Pushed': repo.pushed_at, 'Star Count': repo.stargazers_count}
+		'Date Last Pushed': repo.pushed_at, 'Repo Commits': repo.get_commits().totalCount, 'Star Count': repo.stargazers_count}
 
 	return repoData
 
--- visualisation.py
+++ visualisation.py
@@ -26,10 +26,16 @@ else:
 repoData = repoToData(user)
 
 data = pd.DataFrame.from_dict(repoData, orient = 'index', columns = ["Repository Name", "Programming language", 
-"Date created", "Date Last Pushed", "Star Count"])
+"Date created", "Date Last Pushed", "Repo Commits", "Star Count"])
 
-starCount = data.sort_values(by = ['Star Count'], ascending = False)
-plot = sns.barplot(data = starCount[0:5], x = 'Repository Name', y = 'Star Count', palette = 'bright')
-plot.set_title(user.login + " Repository Star Count")
+# starCount = data.sort_values(by = ['Star Count'], ascending = False)
+# plot = sns.barplot(data = starCount[0:5], x = 'Repository Name', y = 'Star Count', palette = 'bright')
+# plot.set_title(user.login + " Repository Star Count")
+
+# plt.show()
+
+numberOfCommits = data.sort_values(by = ['Repo Commits'], ascending = True)
+plot = sns.barplot(data = numberOfCommits[0:10], x = 'Repo Commits', y = 'Repository Name', palette = 'mako')
+plot.set_title(user.login + " Repository Commits")
 
 plt.show()
